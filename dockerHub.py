7f199ed88a256e81ff0faea25845275c5a03d737
Griffin Puc <griffinpuc@outlook.com>
2019-07-16 15:03:10 -0400

    Added gitignore

--- dockerHub.py
+++ dockerHub.py
@@ -2,6 +2,7 @@ import docker
 
 try:
     client = docker.from_env()
-    client.containers.list()
+    client.images.build(path="./", tag={image_tag})
 except:
-    print("obviously failed")
+    print("No docker environment found. Please install and configure Docker toolbox > https://docs.docker.com/toolbox/toolbox_install_windows/.")
+
--- instrumentInstance.py
+++ instrumentInstance.py
@@ -1,5 +1,20 @@
+import docker
+
 class instrumentInstance:
 
-      def __init__(self, name, age):
+    def __init__(self, name, port, dockerClient):
         self.name = name
-        self.age = age
+        self.port = port
+        self.dockerClient = dockerClient
+
+    def startInstrument(self, type, *delay, **delayParameters):
+
+        autoUpdating = True if type == 'auto' else False
+
+        if autoUpdating & ('delay' in delayParameters):
+            delay = delayParameters['delay']
+        
+        self.dockerClient.containers.run("ubuntu:latest", "sleep infinity", detach=True)
+
+
+
